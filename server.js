1f11333d3ae195822e01869d3a7ef0138ce7e669
Ryan McIntyre <rmcintyre598@gmail.com>
2024-01-29 21:35:54 -0500

    edited the readme

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
- const htmlroutes = require('./routes/htmlRoutes.js');
- const apiroutes = require('./routes/apiRoutes.js');
+const htmlroutes = require('./routes/htmlRoutes.js');
+const apiroutes = require('./routes/apiRoutes.js');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 
@@ -10,8 +10,8 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- app.use(apiroutes);
-  app.use(htmlroutes);
+app.use(apiroutes);
+app.use(htmlroutes);
 
 app.use(express.static("public"));
 app.listen(PORT, () =>
